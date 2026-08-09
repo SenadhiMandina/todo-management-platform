f27739e07d227a0a7c98f32dd18571874f3d6153
Senadhi Mandina <haksenadhimandina123@gmail.com>
2026-08-09 17:23:01 +0530

    did some necessary api changes

--- api/db-client.js
+++ api/db-client.js
@@ -1,31 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
-import { triggerRestore } from './db-wake.js';
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-console.log("SUPABASE URL:", supabaseUrl);
-console.log(
-  "SUPABASE KEY:",
-  supabaseKey ? "FOUND" : "MISSING"
-);
 
 const supabase = createClient(
   supabaseUrl,
-  supabaseKey,
-  {
-    global: {
-      fetch: async (url, options) => {
-        const res = await fetch(url, options);
-
-        if (!res.ok && res.status >= 500) {
-          triggerRestore();
-        }
-
-        return res;
-      },
-    },
-  }
+  supabaseKey
 );
+
 
 export default supabase;
